6e52bf0bdeb584cc2d26439da9734fcc910b92f6
devbini <cong8685@naver.com>
2024-11-18 01:15:06 +0900

    feat: 프론트&백 환경 추가

---
+++ backend/src/main/kotlin/com/sse/demo/SsetestApplication.kt
@@ -0,0 +1,11 @@
+package com.sse.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SsetestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SsetestApplication>(*args)
+}
